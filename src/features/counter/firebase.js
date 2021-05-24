c63d741eef0827bfe7ca951e2994b13eeef0a6aa
FARKADI OUSSAMA <58217000+Farkadi06@users.noreply.github.com>
2021-05-24 15:41:27 +0100

    Setting up the Authentication: Registration

--- src/features/counter/firebase.js
+++ src/features/counter/firebase.js
@@ -1,3 +1,6 @@
+import firebase from 'firebase';
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyDaq59QpYiaZWzSbdGBlBpamBgbuesHTCA",
     authDomain: "netflix-clone-9ae72.firebaseapp.com",
@@ -6,3 +9,10 @@ const firebaseConfig = {
     messagingSenderId: "795840668028",
     appId: "1:795840668028:web:95f24ead04358fa76704f9"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export {auth};
+  export default db;
--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,17 +1,38 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import { auth } from '../features/counter/firebase';
 import './SignUpScreen.css'
 
 function SignUpScreen() {
+    const EmailRef = useRef(null);
+    const PasswordRef = useRef(null)
+
+    const register = (e) => {
+        e.preventDefault();
+
+        auth.createUserWithEmailAndPassword(
+            EmailRef.current.value,
+            PasswordRef.current.value
+        ).then((authUser) => {
+            console.log(authUser);
+        }).catch((error) => {
+            alert(error.message);
+        })
+    };
+
+    const signIn = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <div className="SignUpScreen">
             <form>
                 <h1>Sign In</h1>
-                <input type="Email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
-                <button type="submit">Sign In</button>
+                <input ref={EmailRef} type="Email" placeholder="Email" />
+                <input ref={PasswordRef} type="password" placeholder="Password" />
+                <button onClick={signIn} type="submit">Sign In</button>
                 <h4>
                 <span className='SignUpScreen__gray'>New to Netflix? </span>
-                <span className='SignUpScreen__link'> Sign Up now. </span>
+                <span className='SignUpScreen__link' onClick={register} > Sign Up now. </span>
                   </h4>
             </form>
             
